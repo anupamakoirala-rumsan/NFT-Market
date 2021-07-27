07a12c3794edc6ad3ee037bcefe94d25e67b2f6a
anupamakoirala <anukoirala.96@hotmail.com>
2021-07-27 14:24:21 +0545

    Loader component added

--- client/src/component/Home.js
+++ client/src/component/Home.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Nabbar from "./Nav";
 import {Form,Button,Card} from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Loader from "../utilities/Loader"
+
 
 function Home(){
+const [loading,setLoading] = useState(false);
+
     return(
         <div className="home_page">
             <Nabbar/>
+           
+
             <div className ="upload">
             <Card className="smallcard">
             <Form>
@@ -26,10 +32,12 @@ function Home(){
                     <Form.Control type ="Text" />
                 </Form.Group>
             </Form>
-            <Button>Mint</Button>
-            
+            <Button onClick={(event)=>{
+                setLoading(true)                
+            }}>Mint</Button>
             </Card>
-
+            <Loader 
+            loading={loading}/>
             </div>
         </div>
     )
---
+++ client/src/utilities/Loader.js
@@ -0,0 +1,20 @@
+import React from "react";
+import BarLoader from "react-spinners/BarLoader";
+import { css } from "@emotion/react";
+
+
+function Loader(props){
+    const override = css
+    `display:block;
+    width:60%;
+    margin: 0 auto;
+    
+    
+    `;
+    return(
+        <BarLoader loading={props.loading}  css ={override}size={10}/>
+    )
+
+
+}
+export default Loader;
